a6f1e82a25a24e5c296c5e160adc4bc1ecff7a82
Addison Benzshawel <jbenzshawel@gmail.com>
2016-09-04 20:16:31 -0500

    initial commit

---
+++ RazorEngineCms/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RazorEngineCms.Startup))]
+namespace RazorEngineCms
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
