e3d3e5750894835f4a104965a62fa1226f577077
Addison Benzshawel <jbenzshawel@gmail.com>
2016-11-07 23:42:29 -0600

    fixing UI response after deleting a row in a table

--- RazorEngineCms/Scripts/ListDataTable.js
+++ RazorEngineCms/Scripts/ListDataTable.js
@@ -20,31 +20,60 @@ var ListDataTable = function (paramsObj) {
         logger.logError("Error: paramsObj cannot be null in ListDataTable.");
         return;
     }
-    this.selector = null, this.order = null;
+    this.selector = null, this.order = null, this.dataTable = null;
     if (paramsObj.hasOwnProperty("selector")) {
         this.selector = paramsObj.selector;
     }
 
     if (paramsObj.hasOwnProperty("order")) {
-        this.order = paramsObj.oreder;
+        this.order = paramsObj.order;
     }
 
     if (this.selector != undefined) {
-        this.setupDataTable();
+        this.dataTable = this.setupDataTable();
     }
 }
 
 ListDataTable.prototype.setupDataTable = function () {
-    $(this.selector).DataTable({
+    return $(this.selector).DataTable({
         "order": this.order,
         // callback funciton that hides pagination if only one page of results 
         "drawCallback": function () {
             var api = this.api();
             // try to get display length from api context but fall back to 10 
-            var rowsPerPage = api.context != null && api.context.length > 0 ? api.context[0]._iDisplayLength : 10;
+            var rowsPerPage = api.context[0]._iDisplayLength || 10;
             if (api.data() != null && api.data().length < rowsPerPage + 1) {
                 $(".pagination").hide();
             }
         }
     });
+}
+
+ListDataTable.prototype.getDataTable = function () {
+    if (this.selector != null) {
+        this.dataTable = $(this.selector).DataTable();
+        return this.dataTable;
+    }
+    return null;
+}
+
+// draws a data table corresponding to the selector 
+ListDataTable.prototype.draw = function() {
+    this.getDataTable().draw();
+}
+
+// Removes row in data table with a column value matching the integer passed in
+// @param value is int of value in table column 
+ListDataTable.prototype.deleteRowWithInt = function (value) {
+    var cols = $("#pageListTable td");
+    for (var i = 0, column; column = cols[i++];) {
+        var matchVal = parseInt($(column).text(), 10);
+        if (!isNaN(matchVal) && matchVal === parseInt(value, 10)) {
+            $(column).parent().addClass("remove-this-row"); // add class to target to row
+            break; 
+        }
+    }
+    if ($(".remove-this-row").length > 0) {
+        this.getDataTable().row('.remove-this-row').remove().draw(false);
+    }
 }
