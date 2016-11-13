6ea20acd2b0da52b0f5d29c29029e6c1033f72b5
Addison Benzshawel <jbenzshawel@gmail.com>
2016-11-13 09:22:29 -0600

    working on front end. still has bugs

--- RazorEngineCms/Scripts/ListDataTable.js
+++ RazorEngineCms/Scripts/ListDataTable.js
@@ -15,6 +15,8 @@ $.ajax({
     async: false
 });
 
+// constructor paramsObj is an object with params selector (selector of element) 
+// and order (col number order e.g [[1, "desc" ]]
 var ListDataTable = function (paramsObj) {
     if (typeof (paramsObj) != "object" || paramsObj == null) {
         logger.logError("Error: paramsObj cannot be null in ListDataTable.");
@@ -30,15 +32,14 @@ var ListDataTable = function (paramsObj) {
     }
 
     if (this.selector != undefined) {
-        this.dataTable = this.setupDataTable();
+        this.setupDataTable();
     }
 }
 
 ListDataTable.prototype.setupDataTable = function () {
-    return $(this.selector).DataTable({
-        "order": this.order,
+    var dataTableConfig = {
         // callback funciton that hides pagination if only one page of results 
-        "drawCallback": function () {
+        "drawCallback": function() {
             var api = this.api();
             // try to get display length from api context but fall back to 10 
             var rowsPerPage = api.context[0]._iDisplayLength || 10;
@@ -46,34 +47,72 @@ ListDataTable.prototype.setupDataTable = function () {
                 $(".pagination").hide();
             }
         }
-    });
+    };
+    // add default sort order if set 
+    if (typeof (this.order) === "object") {
+        dataTableConfig.order = this.order;
+    }
+
+    return $(this.selector).DataTable(dataTableConfig);
 }
 
-ListDataTable.prototype.getDataTable = function () {
-    if (this.selector != null) {
-        this.dataTable = $(this.selector).DataTable();
-        return this.dataTable;
+// @param selector (optional) string element selector 
+// returns DataTable object for selector or null
+ListDataTable.prototype.DataTable = function (selector) {
+    // use object property if selector param not set 
+    if (this.selector != null && typeof (selector) === "undefined") {
+        selector = this.selector;
     }
+
+    if (typeof (selector) === "string" & $(selector).length > 0) {
+        return $(selector).DataTable();
+    }
+
     return null;
 }
 
 // draws a data table corresponding to the selector 
-ListDataTable.prototype.draw = function() {
-    this.getDataTable().draw();
+ListDataTable.prototype.draw = function (pageToBegining) {
+    if (typeof (pageToBegining) === "undefined")
+        pageToBegining = false;
+    this.DataTable().draw(pageToBegining);
 }
 
 // Removes row in data table with a column value matching the integer passed in
 // @param value is int of value in table column 
 ListDataTable.prototype.deleteRowWithInt = function (value) {
-    var cols = $("#pageListTable td");
-    for (var i = 0, column; column = cols[i++];) {
-        var matchVal = parseInt($(column).text(), 10);
-        if (!isNaN(matchVal) && matchVal === parseInt(value, 10)) {
-            $(column).parent().addClass("remove-this-row"); // add class to target to row
-            break; 
-        }
+    var className = "remove-this-row";
+    if (this.addClassToRowWithInt(value, className)) {
+        this.DataTable().row("." + className).remove().draw(false);
     }
-    if ($(".remove-this-row").length > 0) {
-        this.getDataTable().row('.remove-this-row').remove().draw(false);
+}
+
+ListDataTable.prototype.copyRowWithInt = function(value) {
+    var className = "copy-this-row";
+    this.addClassToRowWithInt(value, className);
+    var rowData = [];
+    $(className).find('td').each(function () {
+        rowData.push($(this).text());
+    });
+    if (rowData.length > 0) {
+        this.DataTable().row.add(rowData).draw(false);
     }
+}
+
+ListDataTable.prototype.addClassToRowWithInt = function (value, className) {
+    var $cols = $(this.selector + " td");
+    if ($cols.length > 0) {
+        // loop over columns until find match with value (always delete just first row found)
+        var callbackMatches = function(key) {
+            var column = $cols[key];
+            var matchVal = parseInt($(column).text(), 10);
+            if (!isNaN(matchVal) && matchVal === parseInt(value, 10)) {
+                $(column).parent().addClass(className); // add class to target to row
+                return true;
+            }
+            return false;
+        };
+        return Object.keys($cols).some(callbackMatches);
+    }
+    return false;
 }
